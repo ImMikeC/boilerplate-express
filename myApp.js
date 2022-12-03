84b099ca1181e4488c2781432dbd5f72c5d0a96b
José Miguel Correa <69221181+ImMikeC@users.noreply.github.com>
2022-12-03 13:32:29 +0000

    Avances.

--- myApp.js
+++ myApp.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
+
 let express = require('express');
 let app = express();
 
 app.get('/json', (req, res) => {
-  res.status(200).json({"message": "Hello json"})
+  res.status(200).json({"message": "Hello json"}).process.env.MESSAGE_STYLE
 })
 
 
