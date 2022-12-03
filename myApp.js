a3c9ed56e80fa06d8b471b839041ce04ab38f5a9
Replit user <>
2022-12-03 13:06:24 +0000

    Push before create .env file

--- myApp.js
+++ myApp.js
@@ -1,6 +1,16 @@
 let express = require('express');
 let app = express();
 
+app.get('/json', (req, res) => {
+  res.status(200).json({"message": "Hello json"})
+})
+
+
+//app.use(200, {"message": "Hello json"})
+
+
+console.log("Hello World")
+
 
 
 
