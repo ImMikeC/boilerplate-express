a0171a53cda907262f03c7bde734b4292853867d
Replit user <>
2022-12-06 13:49:53 +0000

    .

--- myApp.js
+++ myApp.js
@@ -1,11 +1,20 @@
+process.env.MESSAGE_STYLE
 require('dotenv').config()
 
 let express = require('express');
 let app = express();
 
 app.get('/json', (req, res) => {
-  res.status(200).json({"message": "Hello json"}).process.env.MESSAGE_STYLE
-})
+  var response = {"message": "Hello json"}
+
+  if(process.env.MESSAGE_STYLE==='uppercase'){
+  //Override message attribute value based on condition
+  response.message = response.message.toUpperCase();  
+}
+
+return res.json(response)
+  
+                       })
 
 
 //app.use(200, {"message": "Hello json"})
